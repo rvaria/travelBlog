ddd3628e6fbf0f53e48ddb604ee8ce2c51d84bee
Riddhi <ZHAC110@live.rhul.ac.uk>
2023-12-18 16:22:55 +0000

    Add image slideshow in homepage

--- js/index.js
+++ js/index.js
@@ -1,20 +1,44 @@
 const homeImage = document.querySelectorAll('.imageContainer');
 const blogBoxes = document.querySelectorAll('.blogBox');
+const imageSlides = document.querySelectorAll('.image');
+let current = 1;
 
-const intersectionObserver = new IntersectionObserver(entries =>  {
-    entries.forEach(entry => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('fade');
-        }
+slideshow();
+fadePage();
+
+function fadePage() {
+    const intersectionObserver = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade');
+            }
+        });
+    }, {
+        threshold: 0.5,
     });
-}, {
-    threshold:0.5,
-});
 
-blogBoxes.forEach((blog) => {
-    intersectionObserver.observe(blog);
-});
+    blogBoxes.forEach((blog) => {
+        intersectionObserver.observe(blog);
+    });
 
-homeImage.forEach((image) => {
-    intersectionObserver.observe(image);
-});
+    homeImage.forEach((image) => {
+        intersectionObserver.observe(image);
+    });
+}
+
+function slideshow() {
+
+    if(current > imageSlides.length) {
+        current = 1;
+    }
+
+    for(let i = 0; i < imageSlides.length; i++) {
+        imageSlides[i].style.display = "none"; 
+    }
+
+    imageSlides[current-1].style.display="block";
+
+    current++;
+    setTimeout(slideshow, 1500);
+
+}
