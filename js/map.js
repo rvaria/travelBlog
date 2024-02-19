f2faa1f3351642d85d4658baffc37c078e3ed8a2
Riddhi <ZHAC110@live.rhul.ac.uk>
2024-02-19 19:15:45 +0000

    Add link to marker popups

--- js/map.js
+++ js/map.js
@@ -1,10 +1,18 @@
 var map = L.map('map').setView([30, 0], 2.5);
+map.doubleClickZoom.disable();
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-L.marker([43.4, 16.6]).addTo(map);
-L.marker([-0.02, 37.9]).addTo(map);
-L.marker([48.8, 2.3]).addTo(map);
-L.marker([55.8, -4.2]).addTo(map);
+
+
+var croatia = L.marker([43.4, 16.6]).addTo(map).bindPopup('<a href="../pages/croatia.html">Croatia</a>');
+
+var kenya = L.marker([-4, 39.6]).addTo(map).bindPopup("Kenya");
+
+var paris = L.marker([48.8, 2.3]).addTo(map).bindPopup('<a href="../pages/paris.html">Paris</a>');
+
+var scotland = L.marker([55.8, -4.2]).addTo(map).bindPopup('<a href="../pages/scotland.html">Scotland</a>');
+
+var budapest = L.marker([47.4, 19]).addTo(map).bindPopup('<a href="../pages/budapest.html">Budapest</a>');
