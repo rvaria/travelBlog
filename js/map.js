53b773e1c25eb6f7d34aef3a532313653669212b
Riddhi <ridz.varia@gmail.com>
2024-02-19 19:45:02 +0000

    Add additional marker

--- js/map.js
+++ js/map.js
@@ -10,10 +10,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var croatia = L.marker([43.4, 16.6]).addTo(map).bindPopup('<a href="../pages/croatia.html">Croatia</a>');
 
-var kenya = L.marker([-4, 39.6]).addTo(map).bindPopup("Kenya");
-
 var paris = L.marker([48.8, 2.3]).addTo(map).bindPopup('<a href="../pages/paris.html">Paris</a>');
 
 var scotland = L.marker([55.8, -4.2]).addTo(map).bindPopup('<a href="../pages/scotland.html">Scotland</a>');
 
 var budapest = L.marker([47.4, 19]).addTo(map).bindPopup('<a href="../pages/budapest.html">Budapest</a>');
+
+var kenya = L.marker([-4, 39.6]).addTo(map).bindPopup("Kenya");
+
+var seville = L.marker([37.3, -5.9]).addTo(map).bindPopup("Seville");
