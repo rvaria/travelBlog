abbfad6b660bf21e25f0b26e7baf124a617ae9bb
Riddhi <ridz.varia@gmail.com>
2024-02-25 14:08:51 +0000

    Modify web component to include style and callback

--- js/comment.js
+++ js/comment.js
@@ -1,10 +1,37 @@
 class CommentSection extends HTMLElement {
+
     constructor() {
         super();
+    }
 
-        const template = document.createElement('template');
-
-        template.innerHTML = `
+    connectedCallback() {
+        this.innerHTML = `
+        <style>
+        #comments-container {
+            margin:auto;
+            width:45%;
+            text-align:center;
+            background-color: #f2f2f2;
+            padding:15px;
+        }
+        
+        #name {
+            text-align:left;
+            border-radius: 5px;
+            padding: 20px;
+        }
+        
+        input[type=text] {
+          width: 100%;
+          padding: 12px 20px;
+          margin: 8px;
+          box-sizing: border-box;
+        }
+        
+        button {
+            padding:2%;
+        }
+        </style>
         <div id="comments-container">
             <div id="name">
                 <label for="fname">Enter name</label>
@@ -18,4 +45,7 @@ class CommentSection extends HTMLElement {
         </div>
         `;
     }
-}
+    
+  }
+
+window.customElements.define('comment-section', CommentSection);
