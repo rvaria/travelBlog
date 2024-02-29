c9c303204be9bc091a7f2cf980537ed65e1fd6d5
rvaria <ridz.varia@gmail.com>
2024-02-29 18:51:06 +0000

    Link to browser modules

--- js/firebaseCall.js
+++ js/firebaseCall.js
@@ -1,8 +1,9 @@
-//import firebase from "firebase/app";
-//import "firebase/functions";
+import "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import "https://www.gstatic.com/firebasejs/10.8.1/firebase-functions.js";
+
+console.log("It works a bit?");
 
 export function addComment(blogID, nameInput, commentInput) {
-    alert(nameInput.value);
 
     var addData = firebase.functions().httpsCallable('addData');
     addData({
@@ -12,5 +13,5 @@ export function addComment(blogID, nameInput, commentInput) {
     })
         .then((result) => {
             // Return result from cloud function
-        });
-}
+        }); 
+} 
